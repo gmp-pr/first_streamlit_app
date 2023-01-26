8fa0afc6e4f6e83946b70df779b77b2d04c28b43
Gabriel <72473897+gmp-pr@users.noreply.github.com>
2023-01-26 11:46:38 -0400

    Update streamlit_app.py
    change tile
    change header
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free_Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
