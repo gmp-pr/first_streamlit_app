79a447fc3fbf40de69997a29718fbc3a265e3b8b
Gabriel <72473897+gmp-pr@users.noreply.github.com>
2023-01-26 11:42:53 -0400

    Update streamlit_app.py
    added emojis
    added new text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(':bowl_with_spoon:Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free_Range Egg')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free_Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
