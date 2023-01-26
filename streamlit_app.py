0bf2464836696f863ecda67ed4459ed778fe8588
Gabriel <72473897+gmp-pr@users.noreply.github.com>
2023-01-26 11:35:09 -0400

    Update streamlit_app.py
    emoji shortcut test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text(':bowl_with_spoon:Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free_Range Egg')
